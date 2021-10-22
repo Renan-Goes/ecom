df0100f48ab17dd483c727177ad7ff2922e93be9
Renan <renanagmelo@gmail.com>
2021-10-22 16:32:36 -0300

    fix: error with patch method

--- src/main/java/com/ecom/Controllers/ProductController.java
+++ src/main/java/com/ecom/Controllers/ProductController.java
@@ -19,11 +19,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -99,7 +98,7 @@ public class ProductController {
         return productService.getProductsLikeByName(paging, productName);
     }     
 
-    @RequestMapping(value="/updateproduct/{productId}", method=RequestMethod.PUT)
+    @PatchMapping("/updateproduct/{productId}")
     @ApiOperation(value="Update fields of a product by its id in the url and fields as json", tags={ "Product" })
     @Transactional
     public ResponseEntity<?> updateProduct(@PathVariable String productId, 
